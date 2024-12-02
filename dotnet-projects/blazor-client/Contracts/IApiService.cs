217d8dd3133aa38ddb873a44af836920d19efdd2
Mariete_Colequete <mariocuellarprieto@gmail.com>
2024-12-02 10:13:20 +0100

    ApiServices + Validation for AddCar

--- dotnet-projects/blazor-client/Contracts/IApiService.cs
+++ dotnet-projects/blazor-client/Contracts/IApiService.cs
@@ -6,4 +6,12 @@ public interface IApiService
 {
     Task<string?> AddCarAsync(CarPostModel car);
     Task<string?> AddRentalAsync(RentalDto rental);
+    Task<CarDto[]?> GetAllCarsAsync();
+    Task<RentalDto[]?> GetAllRentalsAsync();
+    Task<RentalDto?> GetRentalByIdAsync(int rentalId);
+    Task<CarDto?> GetCarByRegNumberAsync(int regNumber);
+    Task<string?> UpdateCarAsync(int regNumber, CarPostModel car);
+    Task<string?> UpdateRentalAsync(int id, RentalDto rental);
+    Task<string?> DeleteRentalAsync(int rentalId);
+    Task<string?> DeleteCarAsync(int carId);
 }
